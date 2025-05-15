6b66f16b9fcf838f4526d9d3abd1e2fa1cc9eb14
Youssef HIMMI <72301011+youssefamjad1@users.noreply.github.com>
2025-05-15 21:01:26 +0200

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,14 +1,13 @@
--- Index on User email (already unique, but we create explicitly for clarity)
+-- Create Indexes
 CREATE INDEX idx_user_email ON User(email);
-
--- Index on Booking user_id
 CREATE INDEX idx_booking_user_id ON Booking(user_id);
-
--- Index on Booking property_id
 CREATE INDEX idx_booking_property_id ON Booking(property_id);
-
--- Index on Property host_id
 CREATE INDEX idx_property_host_id ON Property(host_id);
-
--- Index on Property location
 CREATE INDEX idx_property_location ON Property(location);
+
+-- Measure performance using EXPLAIN ANALYZE
+EXPLAIN ANALYZE
+SELECT p.*
+FROM Property p
+JOIN Booking b ON p.property_id = b.property_id
+WHERE b.user_id = '00000000-0000-0000-0000-000000000000';
