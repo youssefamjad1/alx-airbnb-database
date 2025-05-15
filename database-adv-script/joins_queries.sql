74dd144fa16a32be983bccad4a709b6de4df115c
Youssef HIMMI <72301011+youssefamjad1@users.noreply.github.com>
2025-05-15 21:19:54 +0200

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -29,7 +29,9 @@ SELECT
 FROM
     Property p
 LEFT JOIN
-    Review r ON p.property_id = r.property_id;
+    Review r ON p.property_id = r.property_id
+ORDER BY
+    p.property_id;
 
 
 -- 3. FULL OUTER JOIN: Retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
